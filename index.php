e1fa654b766f7792dad828f12f39ac8f1fa6c36f
tunip3 <ts-gaming@outlook.com>
2018-02-19 20:37:14 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo '<li><iframe width="1" height="1" src="//www.youtube.com/embed/u3pQv9mmZMk?rel=0&start=2&autoplay=1&loop=1&playlist=u3pQv9mmZMk" frameborder="0" allowfullscreen></iframe></li>';
 $imagepath="image.jpg";
 $image=imagecreatefromjpeg( $imagepath);
 header('Content-Type: image/jpeg');
